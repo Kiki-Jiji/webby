ee36e277aabf31a7f8500762b2f767f86224736d
Joshua Halls <joshuah40@googlemail.com>
2019-05-23 17:53:25 +0100

    added database and login

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm, NameForm
-
+from flask_login import current_user, login_user
+from app.model import User
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+from flask_login import logout_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,17 +22,32 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invallid username or password!!')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',  title='Sign In', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out')
+    return redirect(url_for('index'))
 
 @app.route('/test')
 def test():
